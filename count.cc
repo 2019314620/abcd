e21c9b1c57a76f67b1c0476d7d792fc69d9e0e48
2019314620 <winbin0924@gmail.com>
2023-09-05 12:02:14 +0900

    modify count.cc

--- count.cc
+++ count.cc
@@ -1,7 +1,8 @@
 include "count.h"
 
 void count_number() {
-	for(int i=1;i<=10;i++)	std::cout << i << std::endl; 
+	for(int i=1;i<=10;i++)	{std::cout << i << std::endl;}
+ 
 	std::cout << 2019314620 << std::endl;
 	return 0;
 }
